91864f41c63bbfcbbadb75298e267a45d0eae45f
rajgow <rajesh42190@gmail.com>
2015-06-22 22:34:50 +0530

    test

--- scripts/appController.js
+++ scripts/appController.js
@@ -39,16 +39,7 @@
 
     appConfig.controller('BankingAccCtrl', function($scope,$state,utilityService,appvalues,appconstants) {
 
-		 $scope.people = [
-    { name: 'Adam',      email: 'adam@email.com',      age: 10 },
-    { name: 'Amalie',    email: 'amalie@email.com',    age: 12 },
-    { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30 },
-    { name: 'Samantha',  email: 'samantha@email.com',  age: 31 },
-    { name: 'Estefanía', email: 'estefanía@email.com', age: 16 },
-    { name: 'Natasha',   email: 'natasha@email.com',   age: 54 },
-    { name: 'Nicole',    email: 'nicole@email.com',    age: 43 },
-    { name: 'Adrian',    email: 'adrian@email.com',    age: 21 }
-  ];
+		 
 	
 	
          /* This call updates the breadcrumbs,
@@ -64,7 +55,19 @@
     });
 
     appConfig.controller('FormCtrl', function($scope, $state, $location, utilityService, appvalues,appconstants) {
-        $scope.formDetails = appconstants[$state.current.name]; $scope.accountId = $state.params.accountId; $scope.application = {}; $scope.customer = { step : 1 };
+        
+		$scope.people = [
+    { name: 'Adam',      email: 'adam@email.com',      age: 10 },
+    { name: 'Amalie',    email: 'amalie@email.com',    age: 12 },
+    { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30 },
+    { name: 'Samantha',  email: 'samantha@email.com',  age: 31 },
+    { name: 'Estefanía', email: 'estefanía@email.com', age: 16 },
+    { name: 'Natasha',   email: 'natasha@email.com',   age: 54 },
+    { name: 'Nicole',    email: 'nicole@email.com',    age: 43 },
+    { name: 'Adrian',    email: 'adrian@email.com',    age: 21 }
+  ];
+		
+		$scope.formDetails = appconstants[$state.current.name]; $scope.accountId = $state.params.accountId; $scope.application = {}; $scope.customer = { step : 1 };
         //$scope.formFields = angular.copy(appvalues.formFields[ $scope.formDetails.formName ]);
 
         utilityService.updateScopeWithLabels($scope, "formFields", { method : "GET", url : appconstants.parseURILabels + appconstants.formFieldsKey }, function(formFields) {
